97a62359713a4aadee6358f0561f8826ea95f8e4
MrYsLab <MisterYsLab@gmail.com>
2026-06-30 10:23:06 -0400

    Resolved Issue #6

--- telemetrix_aio_esp32/ble_aio_transport.py
+++ telemetrix_aio_esp32/ble_aio_transport.py
@@ -15,7 +15,6 @@
  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 """
 
-from bleak import discover
 from bleak import BleakClient
 from bleak import BleakScanner
 import asyncio
